4b284ebca35a9ccc08878e41bf714b855d1b1b74
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2022-08-19 15:27:51 -0500

    Begin with TDD
    I think I must amend the past commits.

--- src/profiles/tests.py
+++ src/profiles/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from profile.models import Profile
 
-# Create your tests here.
+
+class ProfileTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_profile_creation(self):
+        user = Profile.objects.create_user(
+            username='test',
+        )
+        self.assertTrue(isinstance(user, Profile))
