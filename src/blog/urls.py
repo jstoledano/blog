46498424c14c1d560a1f469cf733a6edf0ae467f
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2022-08-20 13:39:32 -0500

    Category Model with 100% of coverage. Also, create view and dummy html template.
    Fix my stupidness

---
+++ src/blog/urls.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+#         app: org.toledano.blogApp
+#      module: blog.urls
+# description: URL patterns form blog app
+#      author: Javier Sanchez Toledano
+#        date: 2022-08-20
+#     licence: MIT
+#      python: 3.10
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('category/', views.CategoryList.as_view(), name='category_list'),
+    path('category/<str:slug>', views.CategoryDetail.as_view(), name='category_detail'),
+]
--- src/blogApp/settings.py
+++ src/blogApp/settings.py
@@ -23,6 +23,7 @@ THIRD_PARTY_APPS = [
 ]
 LOCAL_APPS = [
     'profiles.apps.ProfilesConfig',
+    'blog.apps.BlogConfig',
 ]
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
