0c748044d402eb038c76316cc0ace34c7776c63b
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2023-09-30 12:49:59 -0600

    Robots, ads, bing y keybase files proofs

--- src/blogApp/urls.py
+++ src/blogApp/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView as t
 
 import blog.views
 
 urlpatterns = [
+    path('robots.txt', t.as_view(template_name='extras/robots.txt', content_type='text/plain')),
+    path('ads.txt', t.as_view(template_name='extras/ads.txt', content_type='text/plain')),
+    path('BingSiteAuth.xml', t.as_view(template_name='extras/BingSiteAuth.xml', content_type='text/plain')),
+    path('keybase.txt', t.as_view(template_name='extras/keybase.txt', content_type='text/plain')),
     path('', include(('blog.urls', 'blog'), namespace='blog')),
     path('admin/', admin.site.urls),
 ]
